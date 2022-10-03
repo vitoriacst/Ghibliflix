d494f324ae9cc891d346839950a728b379b7ce2b
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-10-03 20:25:15 -0300

    feat: :sparkles: Criando o componente Header

---
+++ app/src/components/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-full bg-blue-300">
+      <nav class=" border-gray-200 w-full flex  justify-around p-4 flex-wrap">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <a href="#" class="flex items-center">
+            <img
+              src="https://i.pinimg.com/originals/ec/d4/5c/ecd45c33ebd0f8b7df250068d938fe86.png"
+              class="mr-3 h-6 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              GhibliFlix
+            </span>
+          </a>
+        </div>
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+        >
+          Search
+        </label>
+        <div className="relative">
+          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search"
+            required
+          />
+          <button
+            type="submit"
+            className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
